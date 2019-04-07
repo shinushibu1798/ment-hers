572ffbc066952a02078c207cd9d35303bae87af8
shinu <shinu.shibu1798@gmail.com>
2019-04-07 05:04:54 -0700

    added firebase

--- www/server.js
+++ www/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
 app.use(express.static('./'))
 app.get('/', (req, res) => {
 	
